5040b6d40ca090eba53884b8c907b2fcaaa4eab2
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-11-02 20:06:22 +0000

    working through workshops

--- learn-unit-testing/workshop/index.js
+++ learn-unit-testing/workshop/index.js
@@ -3,3 +3,23 @@
 function makeUrl(name) {
   return "https://pokeapi.co/api/v2/" + name;
 }
+
+const searchParamsToObject = string => {
+  const params = new URLSearchParams (string);
+  return Object.fromEntries(params);
+}
+
+const isLeapYear = year => {
+  if (typeof year !== 'number' || year < 0 ) {
+    return 'Error - please enter a number that is over 0';
+  } else if (year % 400 === 0) {
+      return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
--- learn-unit-testing/workshop/index.test.js
+++ learn-unit-testing/workshop/index.test.js
@@ -1,4 +1,4 @@
-const functionWorking = (actual, expected, msg) => {
+const functionEqual = (actual, expected, msg) => {
   if (actual === expected) {
     const defaultMsg = `Expected ${expected} and received ${actual}`;
     console.info('Test passed: ' + (msg || defaultMsg));
@@ -13,3 +13,47 @@ function test(name, testFunction) {
   testFunction();
   console.groupEnd(name);
 }
+
+test("Pikachu link is rendered", () => {
+  const actual = makeUrl('pikachu');
+  const expected = "https://pokeapi.co/api/v2/pikachu";
+  functionEqual(actual, expected);
+  functionEqual(actual, 1);
+})
+
+// Test passed: Expected https://pokeapi.co/api/v2/pikachu and received https://pokeapi.co/api/v2/pikachu
+
+// Test failed: Expected 1 but receieved https://pokeapi.co/api/v2/pikachu instead
+
+
+/////////////
+
+// this FAILS both times because the objects can't match, it needs the key
+test("searchParamsToObject will create an object from querystring", () => {
+  const actual = searchParamsToObject("name=oliver&email=hello@oliverjam.es");
+  const expected = { name: "oliver", email: "hello@oliverjam.es" };
+  functionEqual(actual, expected);
+  functionEqual(actual, 1);
+})
+
+// this passes because of correct key log .name, and .email 
+test("searchParamsToObject will create an object from querystring", () => {
+  const actual = searchParamsToObject("name=oliver&email=hello@oliverjam.es");
+  const expected = { name: "oliver", email: "hello@oliverjam.es" };
+  functionEqual(actual.name, expected.name);
+  functionEqual(actual.email, expected.email);
+  functionEqual(actual, 1);
+})
+
+// Test passed: Expected oliver and received oliver
+// Test passed: Expected hello@oliverjam.es and received hello@oliverjam.es
+
+
+test('isLeapYear will return an error if a string is passed', () => {
+  functionEqual(isLeapYear('This is not a number'), 'Error - please enter a number that is over 0')
+});
+
+// why doesn't this work
+test('isLeapYear will return an error for a number 0 or below', () => {
+  functionEqual(isLeapYear(0), 'Error - please enter a number that is over 0')
+});
